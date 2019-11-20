15f109c66deadf23fc89424a63663bb18405ea16
Stephen Ramthun <stephenramthun@gmail.com>
2019-11-20 19:57:28 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,3 @@
-import tekst from './other/tekstfil.txt'
-import { getTimeOfDay } from './utils';
-import './other/style.css'
-
 const app = document.getElementById('app');
 
 const greetingHeader = document.createElement('h1');
@@ -10,13 +6,5 @@ const greetingContent = document.createElement('h3');
 greetingHeader.appendChild(document.createTextNode('Heisann!'));
 app.appendChild(greetingHeader);
 
-greetingContent.appendChild(document.createTextNode('Håper du har en fin ' + getTimeOfDay().toLowerCase()));
+greetingContent.appendChild(document.createTextNode('Håper du har en fin dag'));
 app.appendChild(greetingContent);
-
-const tekstfil = document.createElement('p');
-tekstfil.appendChild(document.createTextNode(tekst));
-app.appendChild(tekstfil);
-
-const img = document.createElement("img");
-img.src = "./other/clapping.jpg";
-app.appendChild(img);
